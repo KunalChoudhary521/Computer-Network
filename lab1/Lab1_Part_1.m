85aab73ada854341e1681986fb0b296ffe100232
Kunal Choudhary <kunal.choudhary@mail.utoronto.ca>
2017-01-20 14:15:33 -0500

    Fixed plots and added Ex1.3

--- lab1/Lab1_Part_1.m
+++ lab1/Lab1_Part_1.m
@@ -3,7 +3,7 @@ clc;clear all;
 %Reading data from a file
 %Note that time is in micro seconds and packetsize is in Bytes
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[packet_no_p, time_p, packetsize_p] = textread('poisson1.data', '%f %f %f');
+[packet_no_p, time_p, packetsize_p] = textread('poisson3.data', '%f %f %f');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%Exercise 1.2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %The following code will generate Plot 1; You generate Plot2 , Plot3.
@@ -23,15 +23,16 @@ bytes_p=zeros(1,100);
 while time_p(jj)<=initial_p
     jj=jj+1;
 end
+
 while i<=100
-while ((time_p(jj)-initial_p)<=ag_time*i && jj<length(packetsize_p))
-bytes_p(i)=bytes_p(i)+packetsize_p(jj);
-jj=jj+1;
+    while ((time_p(jj)-initial_p)<=ag_time*i && jj<length(packetsize_p))
+        bytes_p(i)=bytes_p(i)+packetsize_p(jj);
+        
+        jj=jj+1;
+    end
+    i=i+1;
 end
-i=i+1;
-end
-avg_1_1_1 = mean(bytes_p);
-var_1_1_1 = var(bytes_p);
+
 
 %%%%%%%%
 subplot(3,1,1);bar(bytes_p);
@@ -61,11 +62,11 @@ jj=jj+1;
 end
 i=i+1;
 end
-avg_1_1_2 = mean(bytes_p);
-var_1_1_2 = var(bytes_p);
 
+t_p2 = [30:0.1:40-0.1];
 %%%%%%%%
-subplot(3,1,2);bar(bytes_p);
+subplot(3,1,2);bar(t_p2, bytes_p);
+xlim([30 42]);  %x-axis starts from 30 seconds, rather than 0
 title('Bytes Arrived in 100ms Intervals');
 xlabel('Interval Number');
 ylabel('Number of Bytes');
@@ -86,11 +87,20 @@ jj=jj+1;
 end
 i=i+1;
 end
-avg_1_1_3 = mean(bytes_p);
-var_1_1_3 = var(bytes_p);
 
+t_p3 = [50.2:0.01:51.2-0.01];
 %%%%%%%%
-subplot(3,1,3);bar(bytes_p);
+subplot(3,1,3);bar(t_p3, bytes_p);
+xlim([50.2 51.4]);  %x-axis starts from 50.2 seconds, rather than 0
 title('Bytes Arrived in 10ms Intervals');
 xlabel('Interval Number');
 ylabel('Number of Bytes');
+
+
+%%%%%%%%%%%%%%1.3: mean bit rate, mean & variance of time difference%%%%%%%%%
+
+time_diff = time_p(2:length(time_p)) - time_p(1:length(time_p)-1);
+mean_time_diff = mean(time_diff);
+vari_time_diff = var(time_diff);
+
+mean_bit_rate = ((mean(packetsize_p) * 8)/mean_time_diff) * 1e6;
