74b7ba35f1b62ac3e688994dc6aac6eb2faebf48
KunalChoudhary521 <kunal.choudhary@mail.utoronto.ca>
2017-02-03 11:51:26 -0500

    Initial commit for part2.

---
+++ lab2a/part2/TrafficGenerator.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+
+class TrafficGenerator
+{
+    public static void main (String[] args)
+    {
+        String fileName = "poisson3.data";
+        BufferedReader bis = null;
+        String currentLine = null;
+        PrintStream pout = null;
+    }
+}
